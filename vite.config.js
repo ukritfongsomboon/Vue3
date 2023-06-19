54bbb17c73f15d85382959549443230b4a410efa
Ukrit Fongsomboon <Ukrit.fb@gmail.com>
2023-06-19 14:56:01 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,12 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
+      // สำหรับต่อ Path สำหรับตัว @
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  server: {
+    // สำหรับเปลี่ยน Port การ Run
+    port: 8080,
   },
 })
